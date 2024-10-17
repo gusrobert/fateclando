b82b037d9863472beb99c6a281c5ccba46224869
Gustavo Moura <gustavorobert84@gmail.com>
2024-10-16 21:42:41 -0300

    Acrescentado o ip de forma dinâmica e a coleta da chave

--- client/client.py
+++ client/client.py
@@ -1,15 +1,19 @@
 import socket
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('localhost', 12345))
+server_ip = input("Digite o IP do servidor: ")
+public_key_path = input("Digite o caminho da chave pública do servidor (arquivo .pem): ")
 
-public_key_bytes = client_socket.recv(1024)
+with open(public_key_path, 'rb') as key_file:
+    public_key_bytes = key_file.read()
+
 public_key = serialization.load_pem_public_key(public_key_bytes, backend=default_backend())
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((server_ip, 12345))  # Conectar ao IP do servidor
 
 while True:
     message = input("Digite a mensagem (ou 'sair' para encerrar): ")
