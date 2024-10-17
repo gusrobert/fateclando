4b92ded5e383751163d673624e6adafa5cc0b8b9
Gustavo Moura <gustavorobert84@gmail.com>
2024-10-16 23:06:23 -0300

    Comentários no código para entrega final

--- client/client.py
+++ client/client.py
@@ -1,24 +1,31 @@
 import socket
 import rsa
 
+# recebo do usuário o endereço de ip do servidor e sua chave pública
 server_ip = input("Digite o IP do servidor: ")
 public_key_path = input("Digite o caminho da chave pública do servidor: ")
 
+# lê a chave pública
 with open(public_key_path, 'rb') as key_file:
     public_key_bytes = key_file.read()
 
+# carrega a chave pública
 public_key = rsa.PublicKey.load_pkcs1(public_key_bytes, format='PEM')
 
+# inicializa o socket, para que possa se conectar com o servidor através da porta
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((server_ip, 12345))  # Conectar ao IP do servidor
+client_socket.connect((server_ip, 5002))
 
 while True:
-    message = input("Digite a mensagem (ou 'sair' para encerrar): ")
+    # recebe a mensagem digitada pelo usuário
+    message = input("Digite a mensagem: ").encode('utf-8')
     if message.lower() == 'sair':
         break
     
+    # criptografa a mensagem utilizando a chave pública do servidor
     encrypted_message = rsa.encrypt(message, public_key)
     
+    # envia a mensagem ao servidor
     client_socket.send(encrypted_message)
 
-client_socket.close()
+client_socket.close() # fecha a conexão
--- server/server.py
+++ server/server.py
@@ -1,35 +1,36 @@
 import socket
 import rsa
 
+# Recebo as chaves privada e pública do servidor
 private_key_path = input('Endereco da chave privada: ')
-public_key_path = input('Endereco da chave pública: ')
 
+# Leio as chaves
 with open(private_key_path, 'r') as key_file:
-    private_key_bytes = key_file.read()
+    private_key_bytes = key_file.read().encode()
 
-with open(public_key_path, 'r') as key_file:
-    public_key_bytes = key_file.read()
-
+# Carrega as chaves
 private_key = rsa.PrivateKey.load_pkcs1(private_key_bytes, format='PEM')
-public_key = rsa.PublicKey.load_pkcs1(public_key_bytes, format='PEM')
 
+# Inicializa o socket e configura o ip e a porta
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('0.0.0.0', 12345))
+server_socket.bind(('0.0.0.0', 5002))
 server_socket.listen(1)
 print("Aguardando conexão...")
 
+# Recebe as conexões
 conn, addr = server_socket.accept()
 print(f"Conectado a {addr}")
 
-conn.send(public_key_bytes)
-
 while True:
+    # recebe as mensagens encriptadas com a chave pública do servidor
     encrypted_message = conn.recv(1024)
     if not encrypted_message:
         break
 
+    # descriptografa a mensagem utilizando a chave privada
     decrypted_message = rsa.decrypt(encrypted_message, private_key)
 
+    #exibe a mensagem na tela
     print(f"Mensagem recebida: {decrypted_message.decode()}")
 
-conn.close()
+conn.close() # fecha a conexão
