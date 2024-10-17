8c282c768f2d590b9267d224b9199273e91b35bd
Gustavo Moura <gustavorobert84@gmail.com>
2024-10-16 22:32:20 -0300

    Aprimorada a criptação e decriptação da mensagem

--- client/client.py
+++ client/client.py
@@ -1,8 +1,5 @@
 import socket
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives import hashes
+import rsa
 
 server_ip = input("Digite o IP do servidor: ")
 public_key_path = input("Digite o caminho da chave pública do servidor: ")
@@ -10,7 +7,7 @@ public_key_path = input("Digite o caminho da chave pública do servidor: ")
 with open(public_key_path, 'rb') as key_file:
     public_key_bytes = key_file.read()
 
-public_key = serialization.load_pem_public_key(public_key_bytes, backend=default_backend())
+public_key = rsa.PublicKey.load_pkcs1(public_key_bytes, format='PEM')
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((server_ip, 12345))  # Conectar ao IP do servidor
@@ -20,14 +17,7 @@ while True:
     if message.lower() == 'sair':
         break
     
-    encrypted_message = public_key.encrypt(
-        message.encode(),
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
+    encrypted_message = rsa.encrypt(message, public_key)
     
     client_socket.send(encrypted_message)
 
--- server/server.py
+++ server/server.py
@@ -1,24 +1,20 @@
 import socket
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives import hashes
+import rsa
 
-private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048,
-    backend=default_backend()
-)
+private_key_path = input('Endereco da chave privada: ')
+public_key_path = input('Endereco da chave pública: ')
 
-public_key = private_key.public_key()
-public_key_bytes = public_key.public_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PublicFormat.SubjectPublicKeyInfo
-)
+with open(private_key_path, 'r') as key_file:
+    private_key_bytes = key_file.read()
+
+with open(public_key_path, 'r') as key_file:
+    public_key_bytes = key_file.read()
+
+private_key = rsa.PrivateKey.load_pkcs1(private_key_bytes, format='PEM')
+public_key = rsa.PublicKey.load_pkcs1(public_key_bytes, format='PEM')
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('0.0.0.0', 12345))  # Escuta em todas as interfaces
+server_socket.bind(('0.0.0.0', 12345))
 server_socket.listen(1)
 print("Aguardando conexão...")
 
@@ -31,14 +27,9 @@ while True:
     encrypted_message = conn.recv(1024)
     if not encrypted_message:
         break
-    decrypted_message = private_key.decrypt(
-        encrypted_message,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
+
+    decrypted_message = rsa.decrypt(encrypted_message, private_key)
+
     print(f"Mensagem recebida: {decrypted_message.decode()}")
 
 conn.close()
